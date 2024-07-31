83332c9aa2b079667ef375ddd0e986883fa2ee71
AlleKusuma <allekusuma059@gmail.com>
2024-07-31 12:56:59 +0530

    first

---
+++ add_Extra1.java
@@ -0,0 +1,18 @@
+class add_Extra1
+{
+	public static void main(String args[])
+	{
+		int n=12391,rem,sum=0,mul=1;
+		while(n!=0)
+		{
+			rem=(n%10)+1;
+			if(rem==10)
+				rem=0;
+			sum=sum+rem*mul;
+			mul=mul*10;
+			n/=10;
+		}
+		System.out.println(sum);
+	}
+	
+}
